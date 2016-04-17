2c8bc32fbe1ebb42e3a0d044f2d192439cd47541
Alessio Biancalana <dottorblaster@gmail.com>
2016-04-17 20:46:55 +0200

    implemented version display

--- lib/burst.rb
+++ lib/burst.rb
@@ -1,27 +1,31 @@
 module Burst
-  class << self
-    def front_matter_generator(options)
-      front_matter = [
-        {label: "layout", content: "post"},
-        {label: "status", content: "publish"},
-        {label: "published", content: "true"},
-        {label: "title", content: options[:title] || ""},
-        {label: "date", content: Time.now.to_s},
-        {label: "categories", content: "\n- Category"},
-        {label: "tags", content: "\n- first_tag\n- second_tag"}
-      ]
-    end
+	class << self
+		def front_matter_generator(options)
+			front_matter = [
+				{label: "layout", content: "post"},
+				{label: "status", content: "publish"},
+				{label: "published", content: "true"},
+				{label: "title", content: options[:title] || ""},
+				{label: "date", content: Time.now.to_s},
+				{label: "categories", content: "\n- Category"},
+				{label: "tags", content: "\n- first_tag\n- second_tag"}
+			]
+		end
 
-    def print_line(line)
-      puts line[:label] + ": " + line[:content]
-    end
+		def print_line(line)
+			puts line[:label] + ": " + line[:content]
+		end
 
-    def go(options, customs)
-      puts "---"
-      front_matter_generator(options).each do |line|
-        print_line(line)
-      end
-      puts "---"
-    end
-  end
+		def version(ver)
+			puts ver
+		end
+
+		def go(options, customs)
+			puts "---"
+			front_matter_generator(options).each do |line|
+				print_line(line)
+			end
+			puts "---"
+		end
+	end
 end
