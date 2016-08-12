30ae578f9af825cf22f1b69672d5caf843aac8a9
Alessio Biancalana <dottorblaster@gmail.com>
2016-08-12 19:17:10 +0200

    default title from config file

--- lib/burst.rb
+++ lib/burst.rb
@@ -1,11 +1,11 @@
 module Burst
   class << self
-    def front_matter_generator(options, customs)
+    def front_matter_generator(options, config, customs)
       front_matter = [
         {label: "layout", content: "post"},
         {label: "status", content: "publish"},
         {label: "published", content: "true"},
-        {label: "title", content: options[:title] || ""},
+        {label: "title", content: options[:title] || config['defaultTitle'] || ""},
         {label: "date", content: Time.now.to_s},
         {label: "categories", content: "\n- Category"},
         {label: "tags", content: "\n- first_tag\n- second_tag"}
@@ -26,9 +26,9 @@ module Burst
 			puts ver
 		end
 
-		def go(options, customs)
+		def go(options, config, customs)
 			puts "---"
-			front_matter_generator(options, customs).each do |line|
+			front_matter_generator(options, config, customs).each do |line|
 				print_line(line)
 			end
 			puts "---"
