61fa4f37bd776628eaf41783857d6c4e58c8e7b0
Alessio Biancalana <dottorblaster@gmail.com>
2016-08-16 19:03:54 +0200

    deleted front_matter_generator function, call to new class

--- lib/burst.rb
+++ lib/burst.rb
@@ -1,23 +1,5 @@
 module Burst
 	class << self
-		def front_matter_generator(options, config, customs)
-			front_matter = [
-				{label: "layout", content: "post"},
-				{label: "status", content: "publish"},
-				{label: "published", content: "true"},
-				{label: "title", content: options[:title] || config['defaultTitle'] || ""},
-				{label: "date", content: Time.now.to_s},
-				{label: "categories", content: "\n- Category"},
-				{label: "tags", content: "\n- first_tag\n- second_tag"}
-			]
-
-			if options[:custom] != ""
-				front_matter = CustomsHandler.populate(front_matter, customs[options[:custom]])
-			end
-
-			return front_matter
-		end
-
 		def print_line(line)
 			puts line[:label].yellow + ": " + line[:content].cyan
 		end
@@ -28,7 +10,7 @@ module Burst
 
 		def go(options, config, customs)
 			puts "---"
-			front_matter_generator(options, config, customs).each do |line|
+			FrontMatter.generate(options, config, customs).each do |line|
 				print_line(line)
 			end
 			puts "---"
